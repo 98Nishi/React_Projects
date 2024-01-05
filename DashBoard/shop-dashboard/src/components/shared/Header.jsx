56424f111417635c677f117b1943846981caee60
98Nishi <nishamaurya7499@gmail.com>
2024-01-05 15:56:22 +0530

    almost dome

--- DashBoard/shop-dashboard/src/components/shared/Header.jsx
+++ DashBoard/shop-dashboard/src/components/shared/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React,{ Fragment} from "react";
 import { HiOutlineBell, HiOutlineChatAlt, HiOutlineSearch } from "react-icons/hi";
-import { Popover } from '@headlessui/react'
+import { Popover, Transition } from '@headlessui/react'
 
 export default function Header(){
     return (
@@ -17,11 +17,30 @@ export default function Header(){
         <Popover className="relative">
         {({ open }) => (
           <>
-            <Popover.Button
-              className="p-1.5 rounded-lg inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-non active:bg-gray-100"
+            <Popover.Button className="p-1.5 rounded-lg inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-non active:bg-gray-100"
             >
                  <HiOutlineChatAlt font-size={24}/>
-            </Popover.Button></>)}
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute right-0 z-10 mt-2.5 w-80">
+                  <div className="bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5">
+                    <strong className="text-gray-700 font-medium">Messages</strong>
+                 <div className="mt-2 py-1 text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, laborum.
+                 </div>
+                  </div>
+                 </Popover.Panel>
+              </Transition>
+            </>
+            )}
 
 </Popover>
            
